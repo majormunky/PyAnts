b7af6c1e333fe615a1666b649f97e50fe7f0b6eb
Josh Bright <josh.bright@gmail.com>
2020-03-02 22:27:25 -0800

    Setup ant to detect jobs within range

--- Ant.py
+++ Ant.py
@@ -4,10 +4,13 @@ from Engine.Config import get_screenrect
 
 
 class Ant:
-    def __init__(self, x, y):
+    def __init__(self, x, y, game):
         self.screenrect = get_screenrect()
         self.size = 5
-        self.range = 20
+        self.range = 50
+        self.current_range = self.size + 1
+        self.scan_speed = 125
+        self.scan_timer = 0
         self.position = pygame.Vector2(x, y)
         self.velocity = pygame.Vector2(random.random(), random.random())
         self.state = "scanning"
@@ -23,7 +26,14 @@ class Ant:
         }
 
     def update(self, dt):
-        pass
+        if self.state == "scanning":
+            self.scan_timer += dt
+            if self.scan_timer > self.scan_speed:
+                self.scan_timer = 0
+                self.current_range += 1
+                if self.current_range > self.range:
+                    print("Unable to find any jobs, moving to searching state")
+                    self.state = "searching"
 
     def update_old(self, dt):
         self.position.x += self.velocity.x * self.speed * dt
@@ -45,6 +55,14 @@ class Ant:
             (int(self.position.x), int(self.position.y)),
             self.size,
         )
+        if self.state == "scanning":
+            pygame.draw.circle(
+                canvas,
+                (255, 255, 255),
+                (int(self.position.x), int(self.position.y)),
+                self.current_range,
+                1,
+            )
 
     def handle_event(self, event):
         pass
