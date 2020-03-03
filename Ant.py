938df84909eee4ebf113bbd2c84b4687d954e253
Josh Bright <josh.bright@gmail.com>
2020-03-02 23:36:59 -0800

    Fixed git issue

--- Ant.py
+++ Ant.py
@@ -1,6 +1,8 @@
+import math
 import pygame
 import random
 from Engine.Config import get_screenrect
+import utils
 
 
 class Ant:
@@ -11,20 +13,23 @@ class Ant:
         self.range = 50
         self.current_range = self.size + 1
         self.current_job = None
+        self.target = None
         self.scan_speed = 125
         self.scan_timer = 0
         self.position = pygame.Vector2(x, y)
         self.velocity = pygame.Vector2(random.random(), random.random())
-        self.state = "scanning"
+        self.state = None
+        self.change_state("scanning", "Ant Init")
         self.colors = {
             "searching": (200, 0, 0),
             "scanning": (0, 0, 200),
             "found": (0, 200, 0),
+            "moving_to_exit": (200, 0, 200),
         }
         self.speeds = {
-            "searching": 0.15,
-            "scanning": 0,
-            "found": 0.35,
+            "searching": 0.05,
+            "found": 0.05,
+            "moving_to_exit": 0.1,
         }
 
     def update(self, dt):
@@ -36,11 +41,52 @@ class Ant:
                 jobs = self.game.get_jobs_in_range(self.position, self.current_range)
                 if jobs:
                     if self.game.request_job(jobs[0]):
+                        # we need to setup our velocity before we switch to
+                        # the found state
                         self.current_job = jobs[0]
-                        self.state = "found"
+                        angle = utils.get_angle(self.position, self.current_job)
+                        rads = math.radians(angle)
+                        vx = self.speeds["found"] * math.cos(rads)
+                        vy = self.speeds["found"] * math.sin(rads)
+                        self.velocity.x = vx
+                        self.velocity.y = -vy
+                        self.change_state("found", "Found Job")
                 if self.current_range > self.range:
-                    print("Unable to find any jobs, moving to searching state")
-                    self.state = "searching"
+                    self.change_state(
+                        "searching",
+                        "unable to find jobs at this position, moving to a new one",
+                    )
+                    rx = random.randint(self.position.x - 50, self.position.x + 50)
+                    ry = random.randint(self.position.y - 50, self.position.y + 50)
+                    print(rx, ry)
+                    self.target = (rx, ry)
+        elif self.state == "found":
+            self.update_position(dt)
+            d = self.get_distance_to("job")
+            if d < 5:
+                self.change_state("moving_to_exit", "Found our job, moving to exit")
+        elif self.state == "searching":
+            self.update_position(dt)
+            d = self.get_distance_to("target")
+            if d < 5:
+                self.target = None
+                self.change_state(
+                    "scanning", "After searching, we found a new spot to scan"
+                )
+
+    def change_state(self, new_state, why):
+        self.state = new_state
+        print("Changed state to: {} ({})".format(self.state, why))
+
+    def get_distance_to(self, job_type):
+        if job_type == "job":
+            return utils.get_distance(self.position, self.current_job)
+        elif job_type == "target":
+            return utils.get_distance(self.position, self.target)
+
+    def update_position(self, dt):
+        self.position.x += self.velocity.x * dt
+        self.position.y += self.velocity.y * dt
 
     def draw(self, canvas):
         pygame.draw.circle(
@@ -57,8 +103,8 @@ class Ant:
                 self.current_range,
                 1,
             )
-        elif self.state == "found":
-            pygame.draw.line(canvas, (255, 255, 255), self.position, self.current_job)
+        # elif self.state == "found":
+        #     pygame.draw.line(canvas, (255, 255, 255), self.position, self.current_job)
 
     def handle_event(self, event):
         pass
