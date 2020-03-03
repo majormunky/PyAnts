e7ae8256ebcfe1c6fc034d4446c0d934e33b32de
Josh Bright <josh.bright@gmail.com>
2020-03-02 22:01:06 -0800

    Updated game to import ant, added some jobs for the ants to do

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import random
 from Engine.Engine import Engine
 from Engine.Config import get_screenrect
+from Ant import Ant
 
 
 class Game:
@@ -13,6 +14,20 @@ class Game:
             ry = random.randint(0, self.screenrect.height)
             new_ant = Ant(rx, ry)
             self.ants.append(new_ant)
+        # right now a job is just a position
+        # we want the ant to pick up the thing
+        # and move it to a spot to drop it off
+        self.jobs = []
+        self.drop_off = pygame.Rect(20, 20, 20, 20)
+        self.create_jobs(5)
+
+    def create_jobs(self, amount):
+        padding = 40
+        while len(self.jobs) < amount:
+            rx = random.randint(padding, self.screenrect.width - padding)
+            ry = random.randint(padding, self.screenrect.height - padding)
+            if (rx, ry) not in self.jobs:
+                self.jobs.append((rx, ry))
 
     def update(self, dt):
         for ant in self.ants:
@@ -22,23 +37,10 @@ class Game:
         for ant in self.ants:
             ant.draw(canvas)
 
-    def handle_event(self, event):
-        pass
+        for job in self.jobs:
+            pygame.draw.rect(canvas, (0, 200, 200), (job[0], job[1], 5, 5))
 
-
-class Ant:
-    def __init__(self, x, y):
-        self.size = 10
-        self.rect = pygame.Rect(x, y, self.size, self.size)
-        self.color = (200, 0, 0)
-
-    def update(self, dt):
-        pass
-
-    def draw(self, canvas):
-        pygame.draw.circle(
-            canvas, self.color, (self.rect.x, self.rect.y), self.rect.width
-        )
+        pygame.draw.rect(canvas, (255, 255, 255), self.drop_off)
 
     def handle_event(self, event):
         pass
